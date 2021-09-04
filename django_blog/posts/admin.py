87e675b2da4d43877da4e3363d813da6e9c64f22
lea.arturi <lea.arturi@gmail.com>
2021-09-04 11:49:54 -0300

    first commit

---
+++ django_blog/posts/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from posts.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'description',
+        'created_at'
+    ]
